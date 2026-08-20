203211518d3ef605c449e05963bbe5b17c8cc960
Barry Gamache <barry@smashberries.com>
2026-08-20 12:44:58 -0500

    Export nightly operations interfaces

--- apps/restaurantos/src/restaurantos/__init__.py
+++ apps/restaurantos/src/restaurantos/__init__.py
@@ -1,5 +1,12 @@
 from restaurantos.cli import morning_brief
 from restaurantos.importer import RestaurantRecord, import_restaurant_csv
+from restaurantos.nightly import (
+    CompLine,
+    FeatureSale,
+    NightlyReport,
+    normalize_nightly_report,
+)
+from restaurantos.nightly_email import parse_nightly_email
 from restaurantos.tabc_importer import (
     TABCRecord,
     import_tabc_csv,
@@ -7,11 +14,16 @@ from restaurantos.tabc_importer import (
 )
 
 __all__ = [
+    "CompLine",
+    "FeatureSale",
+    "NightlyReport",
     "OperationalRecord",
     "RestaurantRecord",
     "TABCRecord",
     "import_restaurant_csv",
     "import_tabc_csv",
     "morning_brief",
+    "normalize_nightly_report",
     "normalize_tabc_records",
+    "parse_nightly_email",
 ]
